e5c1601c51dfe5704760e16181f923b20b9f45f2
Garvit Chittora <garvitchittora2016@gmail.com>
2021-11-15 22:04:35 +0530

    requirements and UI form setup

--- stockPricePredictor/core/views.py
+++ stockPricePredictor/core/views.py
@@ -1,3 +1,30 @@
+# package imports
+from keras.layers import LSTM, Dense, Dropout
+from pyexcel_xlsx import get_data as xlsx_get
+from sklearn import linear_model
+import matplotlib.dates as mdates
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# django imports
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def getResults(request):
+
+    fields = {}
+
+    if request.method == "POST":
+        data = request.FILES.get('excel')
+        data = xlsx_get(data)
+        model = request.POST.get('model')
+        print(data, model, request.POST)
+
+    return render(request, 'results.html', fields)
--- stockPricePredictor/stockPricePredictor/urls.py
+++ stockPricePredictor/stockPricePredictor/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import getResults
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('results/', getResults),
 ]
