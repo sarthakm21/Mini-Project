2489ac42d5e9b8e842ff093c2f3fdc1ab9984e02
Shreyas Gupta <technology.shreyas@gmail.com>
2021-11-22 17:38:55 +0530

    results

---
+++ AccuracyStats/automate.py
@@ -0,0 +1,24 @@
+import itertools
+from funcs import stackedLSTM, biDirectionalLSTM, classicLSTM
+import json
+
+features = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+result = {}
+
+def featuresetToString(featureset):
+    return ', '.join(featureset)
+
+for L in range(0, len(features)+1):
+    for subset in itertools.combinations(features, L):
+        featureset = list(subset)
+        if len(featureset)<=1:
+            continue
+        print(featureset)
+        with open('../dataset/CIPLA.csv', mode='r') as csv_file:
+            res = stackedLSTM(csv_file, featureset)
+            result[featuresetToString(featureset)] = res['r2_test']
+        with open('stackedResults2.json', 'w+') as f:
+            json.dump(result, f, indent=4)
+
+        
